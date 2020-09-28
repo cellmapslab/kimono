4ba5bf0cc948a42dcea8253367d1bb46bd6de25e
Felicitas Tengen <felicitastengen@Felicitass-MBP.fritz.box>
2020-09-28 12:59:15 +0200

    Shiny App - Uploading Files and showing their dimensions

---
+++ shiny/KiMONo/app.R
@@ -0,0 +1,62 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+
+# Define UI for application
+ui <- fluidPage(
+
+    titlePanel("KiMONo"),
+
+    sidebarPanel(
+        fileInput("input_file", "Upload your different Omics Layers here", accept = c(".csv", ".tsv"), multiple= TRUE),
+
+        fileInput("mapping_file", "Upload your Mapping Information here", accept = c(".csv", ".tsv"), multiple= TRUE),
+
+    ),
+
+    mainPanel(
+        tableOutput("contents")
+    )
+
+
+
+)
+
+# Define server logic required
+server <- function(input, output) {
+
+    output$contents <- renderTable({
+        file <- input$input_file
+        print(file$name)
+
+        ext <- tools::file_ext(file$datapath)
+
+
+        input_tab <- NULL
+        req(file)
+        validate(need(ext == c("csv","tsv"), "Please upload a csv or tsv file"))
+
+
+        for (i in 1:length(file$name)){
+            x <- dim(read_delim(file$datapath[i], delim = " "))
+            x <- append(x, file$name[i], after = 0)
+
+            input_tab <- rbind(input_tab,x )
+        }
+        rownames(input_tab) <- NULL
+        colnames(input_tab) <- c("Filename", "Rows", "Columns")
+
+        input_tab
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
