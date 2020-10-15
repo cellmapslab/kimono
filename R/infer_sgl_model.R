8d9d6de0b6086df5406c7df869861b227db597ed
Felicitas Tengen <felicitastengen@Felicitass-MBP.fritz.box>
2020-10-15 09:12:45 +0200

    implementation of stability selection

--- R/infer_sgl_model.R
+++ R/infer_sgl_model.R
@@ -1,61 +1,61 @@
 
 #' Calculate tau based on frobenius norm
-#' 
+#'
 #' @parameter x matrix/dataframe. must have at least rows & cols > 2
 #' @return Frobenius norm of x
-#' @examples 
+#' @examples
 #' x <- matrix(1:20,5,4)
 #' calc_tau(x)
 calc_tau <- function(x){
   fr_norm <- calc_frobenius_norm(x)
-  10^(-fr_norm) 
+  10^(-fr_norm)
 }
 
 #' Calculate alpha based on frobenius norm and group information
-#' 
+#'
 #' @parameter x matrix/dataframe. must have at least rows & cols > 2
 #' @parameter groups, vector of group numbers
 #' @return Frobenius norm of x
-#' @examples 
+#' @examples
 #' x <- matrix(1:20,5,4)
 #' group <- c(1,1,2,2)
 #' calc_alpha(x, group)
 calc_alpha <- function(x, group){
 
   fr_norm <- c()
-  for (g in unique(group)) { 
+  for (g in unique(group)) {
     tmp <-  calc_frobenius_norm(as.matrix(x[, which(group == g)]))
     fr_norm <- c( fr_norm, tmp )
   }
-  10^mean( -fr_norm, na.rm = T) 
+  10^mean( -fr_norm, na.rm = T)
 
 }
 
 #' Calculate lambda1.se model for crossvalidation
-#' 
+#'
 #' @parameter lambdas vector of lambdas
 #' @parameter cv_result, matrix with nrow == folds and ncol ==  length(lamdas)
 #' @return lambda1.se
 calc_lambda1.se <- function(lambdas,error_cv){
-  
+
   cv <- colMeans(error_cv)
   std         <-  sd(cv) / sqrt(sum(!is.na(cv)))
   lambdas[ min(which(cv <= (min(cv) + std) )) ]
 }
 
 #' Extracts groups numbers
-#' 
-#' @parameter col names vector like 
+#'
+#' @parameter col names vector like
 #' @return vector of numeric groups
-#' @examples 
+#' @examples
 #' names <- c("methylation___cg0123123","rppa___MTOR")
 #' infer_prior_groups(names)
 parse_prior_groups <- function(names, sep="\\___"){
   as.numeric( as.factor( do.call( rbind, strsplit( names , split = sep ))[,1]))
 }
 
-#' detects non informative features 
-#' 
+#' detects non informative features
+#'
 #' @parameter matrix x
 #' @return vector of bool
 #' @export
@@ -63,170 +63,170 @@ is_underpowered <- function(x){
   apply( x , 2, var) == 0
 }
 
-#' detects non informative features 
-#' 
+#' detects non informative features
+#'
 #' @parameter Y_hat predicted matrix with one y_hat per column
 #' @return vector of mse
 calc_cv_error <- function(Y_hat,y){
 
-  apply( Y_hat , 2, calc_mse, y ) 
+  apply( Y_hat , 2, calc_mse, y )
 }
 
-#' detects non informative features 
-#' 
+#' detects non informative features
+#'
 #' @parameter Y_hat predicted matrix with one y_hat per column
 #' @return vector of mse
 parsing_name <- function(string,sep="___"){
-  
+
   idx <- grepl("___",string) #might be variables like intercept which do not have any prefix
- 
-  result <- data.frame('prefix'= as.character(string), "id"= as.character(string), stringsAsFactors = FALSE) 
+
+  result <- data.frame('prefix'= as.character(string), "id"= as.character(string), stringsAsFactors = FALSE)
   split_string <- do.call(rbind,strsplit(as.character(string[idx]),'___'))
   result[-idx,1] <- split_string[,1]
   result[-idx,2] <- split_string[,2]
-  
+
   result
 }
 
 #' Calculate crossvalidation to estimate lambda1.se & identify potential underpowered features
-#' 
+#'
 #' @parameter y data.table - feature to predict
 #' @parameter X data.table - input features with prior names attached to features
 #' @parameter model - input features with prior names attached to features
-#' @parameter intercept boolean 
+#' @parameter intercept boolean
 #' @parameter seed_cv int - remove randomness for crossvalidation
 #' @parameter folds_cv int - defining the amount of folds
 #' @return list containing lambda1.se and feature names excluding underpowered ones
 calc_cv_sgl <- function(y, x, model = "sparse.grp.lasso", intercept = TRUE, seed_cv = 1234, folds_cv = 5){
-  
+
   error_cv    <- matrix(NA, ncol=100, nrow = folds_cv) # matrix storing cv errors. oem tests for 100 lambda values
   repeat_cv   <- TRUE
-  
+
   while (repeat_cv) {
-    
-   
+
+
     if(ncol(x) < 2) # in case we exclude all features return empty list
-      return(list()) 
-    
+      return(list())
+
     set.seed(seed_cv) # set seed to reproduce cv folds
     fold_idx <- sample( rep( 1:folds_cv, length.out = nrow(x) ) )
-    
+
     for (fold in 1:folds_cv) {
-      
+
       #define test and training sets
-      test_x  <- x[which(fold_idx == fold), ]    
+      test_x  <- x[which(fold_idx == fold), ]
       test_y  <- y[which(fold_idx == fold)]
-      
-      train_x <- x[which(fold_idx != fold), ]  
+
+      train_x <- x[which(fold_idx != fold), ]
       train_y <- y[which(fold_idx != fold)]
-      
+
       #remove underpowered features by testing the variance in training and test set
       underpowered <- is_underpowered( test_x) |  is_underpowered( train_x)
-      
+
       x <- x[ , !underpowered, with=FALSE ]
-      
+
       #restart cv if underpowered features were detected
       if( any( underpowered ))
-        break   
-         
+        break
+
       #transform data table to matrix for calculations
       train_x <- as.matrix(train_x)
       train_y <- as.matrix(train_y)
       test_x  <- as.matrix(test_x)
       test_y  <- as.matrix(test_y)
-      
+
       #prepare oem parameters
       group <- parse_prior_groups(colnames(x))
       tau   <- calc_tau(train_x )
       alpha <- calc_alpha(train_x, group)
-      
-      # supressing warnings since oem warns for all "p > n -> it might be slow for small sample sizes". 
+
+      # supressing warnings since oem warns for all "p > n -> it might be slow for small sample sizes".
       # however still the best package for sparse group lasso
-      fit_cv <- suppressWarnings( 
+      fit_cv <- suppressWarnings(
         oem(x = train_x ,
-            y = train_y , 
+            y = train_y ,
             penalty =  model,
             alpha = alpha,
             tau = tau ,
-            groups = group, 
+            groups = group,
             intercept = intercept)
        )
 
       Y_hat             <- predict( fit_cv, test_x, type = "response" ) # predict test set
-      error_cv[fold, ]  <- calc_cv_error(Y_hat,test_y )                 # evaluate test error 
+      error_cv[fold, ]  <- calc_cv_error(Y_hat,test_y )                 # evaluate test error
     }
-    
+
     #stop loop if no features got removed without error
-    repeat_cv <- ifelse(ncol(x) == ncol(train_x),  FALSE, TRUE) 
+    repeat_cv <- ifelse(ncol(x) == ncol(train_x),  FALSE, TRUE)
   }
-  
+
   lambda1.se  <- calc_lambda1.se(fit_cv$lambda[[1]], error_cv)
   features    <- colnames(x)
-  
+
   list("lambda1.se" = lambda1.se,
        "features" = features )
 }
 
 #' Calculate crossvalidation to estimate lambda1.se & identify potential underpowered features
-#' 
+#'
 #' @parameter y data.table - feature to predict
 #' @parameter X data.table - input features with prior names attached to features
 #' @parameter model string - which model to train. currently only sparse group lasso tested
 #' @return edge list for a given input y and x
-train_kimono_sgl  <- function(y, x, model = "sparse.grp.lasso", intercept = TRUE, ...){ 
-  
+train_kimono_sgl  <- function(y, x, model = "sparse.grp.lasso", intercept = TRUE, ..., seed_cv = 1234){
+
   y <- data.table(scale(y))
   x <- data.table(scale(x))
-  
-  # estimate best lambda and identify underpowered features 
-  cv_result   <- calc_cv_sgl(y, x )
-  
+
+  # estimate best lambda and identify underpowered features
+  cv_result   <- calc_cv_sgl(y, x , seed_cv = seed_cv)
+
   if(length(cv_result)==0) return(c()) #exit function hyere if all features got excluded
-  
+
   # parse cv results
   feature_set <- cv_result$features
   lambda1.se  <- cv_result$lambda
-  
+
   # exclude underpowered features and convert input to matrix
   x <- x[,colnames(x) %in% feature_set, with = FALSE]
   x <- as.matrix(x)
-  y <- as.matrix(y) 
-  
+  y <- as.matrix(y)
+
   # get sgl input parameters
   group <- parse_prior_groups(colnames(x))
   tau   <- calc_tau(x)
   alpha <- calc_alpha(x, group)
-  
-  #fit model with supressed warnings on whole dataset. 
-  # supressing warnings since oem warns for all "p > n -> it might be slow for small sample sizes". 
+
+  #fit model with supressed warnings on whole dataset.
+  # supressing warnings since oem warns for all "p > n -> it might be slow for small sample sizes".
   # however still the best package for sparse group lasso
-  fit <-suppressWarnings( 
+  fit <-suppressWarnings(
     oem(x = x ,
-        y = y , 
+        y = y ,
         penalty =  model,
         lambda = lambda1.se ,
         alpha = alpha,
         tau = tau ,
-        groups = group, 
+        groups = group,
         intercept = intercept #oem performs better with intercept even though
                               # we scaled the input data. Inferred intercepts
                               # can be ignored since they are almost 0.
         )
   )
-  
+
   y_hat <- predict(fit, x, type = "response")
-  
+
   covariates  <- rownames(fit$beta$sparse.grp.lasso)
   beta        <- as.vector(fit$beta$sparse.grp.lasso)
   performance <- calc_r_square(y, y_hat )
   mse <- calc_mse(y,y_hat)
-  
+
   #return c() if fit is an intercept only models
   if(!any(beta[covariates != "(Intercept)"] != 0)) return(c())
 
-  
+
   prefix_covariates <- parsing_name(covariates)
-  
+
   data.frame("predictor"=prefix_covariates$id,
                       "value"=beta,
                       "performance"= performance,
@@ -235,7 +235,7 @@ train_kimono_sgl  <- function(y, x, model = "sparse.grp.lasso", intercept = TRUE
                       )
 }
 
-#for debugging purpose only 
+#for debugging purpose only
 #DEBUG <- train_kimono_sgl(y,x)
 #cat("performance:",DEBUG[1,3],"\n features:",length(which(DEBUG[,2]!= 0)),"of:",length(DEBUG[,2]),"\n","mse:",DEBUG[1,4])
 #model = "sparse.grp.lasso"
--- R/kimono.R
+++ R/kimono.R
@@ -1,10 +1,108 @@
+stability_select <- function(x,y, target, nseeds = 50){
+
+  #Initialize the Seeds and empty Matrix and Vectors for mse and rsquared of length 100
+  seeds <- 1:nseeds
+
+  mse_values <- rep(NA, nseeds)
+  rsquared_values <- rep(NA, nseeds)
+
+  # Groupsparse does not always return all features - therefore we need a vector with all the names
+  # and all the relations to calculate the stability selection
+
+  colnames <- colnames(x)
+  names <- sapply(colnames, FUN =function(x) {
+    split <- unlist(str_split(string = x, pattern = "___")[[1]][2])
+    return(split)
+  })
+  names <- c("(Intercept)", unname(names))
+  art <- sapply(colnames, FUN =function(x) {
+    split <- unlist(str_split(string = x, pattern = "___")[[1]][1])
+    return(split)
+  })
+  art <- c("(Intercept)", unname(art))
+
+
+
+  coef_matrix <- c()
+
+  # Iterate over each of the seeds and set the seed
+  for(i in 1:nseeds){
+
+
+    # Based on the Seed calculate the Cross Validation to determine the best lambda
+    # and calculate the best final fit
+
+
+    fit <- train_kimono_sgl(y,x, seed_cv = seeds[i])
+    #print(fit)
+
+    # For some seeds groupsparse does not return a model these have to be skipped
+    if(is.null(fit)){
+      mse_values[i] <- NA
+      rsquared_values[i]<- NA
+
+      next
+    }
+    fit <- fit %>% rename(r_squared = performance )
+
+    # generate a boolean vector with true values for the features that have an influence
+    imp_features <- fit %>% filter(value != 0) %>% pull(predictor)
+    fit_features <- names %in% imp_features
+
+
+    # Calculate the logical vector which features are not 0 in the model
+    # bind this vector to the coefficient matrix
+
+    #coef_matrix <- base::cbind(coef_matrix, unname(as.matrix(fit[,"value"]!=0)) )
+    coef_matrix <- base::cbind(coef_matrix, fit_features)
+
+    # Calculate R-Squared and MSE by comparing real values to the values predicted by the model
+    #y_hat <- as.vector(predict(fit, s =best_lambda, newx=x))
+    mse_values[i] <- fit$mse[1]
+    rsquared_values[i]<- fit$r_squared[1]
+
+  }
+
+  rownames(coef_matrix) <- names
+
+  #print(coef_matrix)
+
+
+  # Dataframe with columns of the target and predictor
+  # The value is the frequency of a feature being included in the different seed models
+  # Overall R-Squared and MSE are the averages of all R-Squared and MSEs of the different seed models
+  # Selected R-Squared and MSE are the averages of the seed models where at least one feature was selected
+
+  fit_df <- tibble(
+    target = rep(target, nrow(coef_matrix)),
+    predictor = rownames(coef_matrix),
+    value = rowSums(coef_matrix)/ncol(coef_matrix),
+    nr_of_col = ncol(coef_matrix),
+    overall_rsq = mean(rsquared_values, na.rm = T),
+    selected_rsq = mean(rsquared_values[colSums(coef_matrix) != 1],  na.rm=T),
+    overall_mse = mean(mse_values, na.rm= T),
+    selected_mse = mean(mse_values[colSums(coef_matrix) != 1], na.rm =T),
+  )
+
+  fit_df$relation = art
+
+
+  return(fit_df)
+
+}
+
+
+
+
+
+
 #' extracting the relevant mapping information
-#' 
-#' @param node , String  
+#'
+#' @param node , String
 #' @param mapping  ,  data.table with ids in thecolumns 1:2
 #' @return vector of feature names
 fetch_mappings <- function(node, mapping){
-    
+
   colnames(mapping) <- c('V1','V2')
   #extract mapped features
   features <- rbind(  subset( mapping, V1  == node)[, 2],
@@ -14,140 +112,165 @@ fetch_mappings <- function(node, mapping){
 }
 
 #' using the prior information to fetsh the right data for X
-#' 
-#' @param node , 
-#' @param input_list , 
-#' @param mapping_list , 
+#'
+#' @param node ,
+#' @param input_list
+#' @param mapping_list ,
 #' @param main_layer - default = 1
 #' @return X sample x feature matrix
-fetch_var <- function(node ,input_list, mapping_list, metainfo, main_layer = 1, sep = "___"){  
-   
+fetch_var <- function(node ,input_list, mapping_list, metainfo, main_layer = 1, sep = "___"){
+
   x <- c()
-  
-  #iterate over all available mappings to fetch features across all levels 
+  #print(paste("Node", node, sep = " "))
+  #print(paste("Main Layer ",main_layer))
+
+  #iterate over all available mappings to fetch features across all levels
   for (mapping_idx in 1:length(mapping_list) ) {
-    
+    #print(mapping_idx)
+
     mapping_to    <- metainfo$main_to[mapping_idx]  # mapping main layer to X
     mapping_name  <- metainfo$ID[mapping_idx]       # mapping name
-  
+
+    #print(mapping_to)
+    #print(mapping_name)
+
+    #print(input_list[[mapping_to]])
+
     all_features <- colnames(input_list[[ mapping_to ]]) # all possible input features
-  
+    #print(all_features)
+
     #if a mapping_list is NULL we map all features (i.e. for clinical data)
-    if( ncol( mapping_list[[mapping_idx]] ) != 0 ){ 
+    if( ncol( mapping_list[[mapping_idx]] ) != 0 ){
         features <- fetch_mappings(node , mapping_list[[mapping_idx]] )
     }else{
-        features <- all_features 
+        features <- all_features
     }
-    
+
     # if main layer is having a prior to itself it might happen that y is in x
-    if( mapping_to == main_layer ) 
-      features <- features[features !=  node]    
-    
+    if( mapping_to == main_layer )
+      features <- features[features !=  node]
+
     #check if there are actually features left
-    if(length(features) == 0) 
+    if(length(features) == 0)
       next
-    
+
+    #print(features)
     #extract relevant data
     data <- input_list[[ mapping_to ]][, features[features %in% all_features] , with = FALSE ]
-    
+
+
+
     #if we have data to add
     if(ncol(data) != 0){
       colnames(data) <- paste0(mapping_name,sep,colnames(data) )
       x <- cbind(x, data)
     }
   }
-  
+
+  #print(x)
+
   y <- input_list[[main_layer]][ , node, with=FALSE]
-  
+
   list("y"=y,
        "x"=x)
 }
 
-#' remove na, scale  
-#' 
+#' remove na, scale
+#'
 #' @param y , vector of doubles
 #' @param x , matrix features in columns and samples in rows
 #' @return x, y without na's
 preprocess_data <- function(y, x){
-  
+
   y <- scale(y)
   x <- scale(x)
-  
+
   tmp_length <- length(y)
-  
+
   x <- x[!is.na(y), , drop = FALSE]
   y <- y[!is.na(y), drop = FALSE]
-  
+
   if(!is.null( dim(x) ) )
     x <- x[ ,!is.na(colSums(x)),drop = FALSE]
-  
+
   list("y"=as.data.table(y),
        "x"=as.data.table(x))
 }
 
-#' check if data is valid  
-#' 
+#' check if data is valid
+#'
 #' @param min_features , default 5
 #' @param x , matrix features in columns and samples in rows
 #' @return TRUE / FALSE
 is_valid <- function( x, min_features  ){
-  
+
   if( ncol(x) < min_features )
     return(FALSE)
-  
+
   if( sum(is.na(colSums( as.matrix(x) ))) > ncol(x)-min_features)
     return(FALSE)
-  
+
   TRUE
 }
 
-#' Infers a model for each node in the main layer 
-#' 
-#' 
+#' Infers a model for each node in the main layer
+#'
+#'
 #' @param input_list - list of omics data. First list element will be used as predictor
 #' @param mapping_list  - list of mappings between each data type one
 #' @param metainfo  - table of relation between mappings and input list
 #' @param main_layer - which input data set represents the main layer (default = 1)
-#' @param min_features - autoexclude models with less than 2 features (default = 2) 
+#' @param min_features - autoexclude models with less than 2 features (default = 2)
 #' @return a network in form of an edge table
-infer_network <- function(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2) {
+infer_network <- function(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2, stab_sel = FALSE) {
 
   node_list <- colnames(input_list[[main_layer]]) #iterating ofer node_list of main layer
-  
+
   foreach(node = node_list, .combine = 'rbind')  %do% {
-    
+
     #get y and x for a given node
-    var_list <- fetch_var(node, 
-                          input_list, 
-                          mapping_list, 
+    var_list <- fetch_var(node,
+                          input_list,
+                          mapping_list,
                           metainfo)
-    
+
     #remove na and scale data
     var_list <- preprocess_data(var_list$y,var_list$x)
-    
+
     #if not enough features stop here
-    if(!is_valid(var_list$x,min_features)) 
+    if(!is_valid(var_list$x,min_features))
       return()
-    
-    #run model in case the model bugs out catch it     
+
+    #run model in case the model bugs out catch it
     possible_error <- tryCatch(
       {
-        subnet <- train_kimono_sgl(var_list$y,var_list$x )
+        if(stab_sel == FALSE){
+          subnet <- train_kimono_sgl(var_list$y,var_list$x )
+        }
+        else{
+        subnet <- stability_select(x = var_list$x, y = var_list$y, target = node )
+        }
         FALSE
       },
       error=function(cond) {TRUE},
       warning=function(cond) {TRUE}
     )
-    
-    if(possible_error) 
+
+    if(possible_error)
       return( )
-    
-    if(is.null(subnet)) 
-      return( )
-    
+
+    if(is.null(subnet))
+       return( )
+
+
+    if(stab_sel==F){
     data.table('target'=node, subnet)
+    }
+    else{
+    subnet
+    }
   }
-  
+
 }
 
 #' Run Kimono - Knowledge-guIded Multi-Omic Netowrk inference
@@ -155,28 +278,28 @@ infer_network <- function(input_list, mapping_list, metainfo,  main_layer = 1, m
 #' @importFrom data.table as.data.table
 #' @importFrom data.table data.table
 #' @import dplyr
-#' @import foreach 
+#' @import foreach
 #' @import oem
 #' @param input_list - list of omics data. First list element will be used as predictor
 #' @param mapping_list  - list of mappings between each data type one
 #' @param main_layer - which input data set represents the main layer (default = 1)
-#' @param min_features - autoexclude models with less than 2 features (default = 2) 
+#' @param min_features - autoexclude models with less than 2 features (default = 2)
 #' @param core - if core != 1 kimono will perform an parallell computation
 #' @return a network in form of an edge table
 #' @export
-kimono <- function(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2,...){
-  
-  result <- infer_network(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2)
-  
+kimono <- function(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2,stab_sel = FALSE ,...){
+
+  result <- infer_network(input_list, mapping_list, metainfo,  main_layer = 1, min_features = 2, stab_sel = stab_sel)
+
   if(nrow(result) == 0)
     warning('model was not able to infer any associations')
-  
+
   result
 }
- 
+
 #source(file = 'R_package/kimono/R/infer_sgl_model.R')
 #source(file = 'R_package/kimono/R/utility_functions.R')
-#main_layer = 1 
+#main_layer = 1
 #node='A1BG'
 #min_features = 2
 #cores = 1
